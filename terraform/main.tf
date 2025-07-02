b98ed81121cb9138cc2e6535be5c4086a070baf4
Rakesh Revashetti <122523208+rakeshrevashetti@users.noreply.github.com>
2025-07-03 01:38:52 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,19 +1,29 @@
 provider "aws" {
-  region = "us-east-1"
+  region = var.aws_region
 }
 
 module "network" {
-  source = "./ecs/vpc.tf"
+  source = "./ecs/vpc"
 }
 
 module "ecs_cluster" {
-  source = "./ecs/ecs_cluster.tf"
+  source = "./ecs/ecs_cluster"
+  ecs_cluster_name = var.ecs_cluster_name
 }
 
 module "alb" {
-  source = "./ecs/alb.tf"
+  source = "./ecs/alb"
+  vpc_id            = module.network.vpc_id
+  public_subnet_ids = module.network.public_subnet_ids
+  alb_sg_id         = module.network.alb_sg_id
 }
 
 module "ecs_app" {
-  source = "./ecs/task_definition.tf"
+  source                 = "./ecs/task_definition"
+  cluster_name           = module.ecs_cluster.ecs_cluster_name
+  task_exec_role_arn     = var.task_exec_role_arn
+  ecr_image              = var.ecr_image
+  subnets                = module.network.public_subnet_ids
+  security_group_id      = module.network.ecs_sg_id
+  alb_target_group_arn   = module.alb.target_group_arn
 }
