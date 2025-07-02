6aecb508197c71ea29dd2ba78dbd07c9dc90770e
Rakesh Revashetti <122523208+rakeshrevashetti@users.noreply.github.com>
2025-07-03 01:36:37 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,19 @@
-provider aws {
-         region = "us-east-1"
-         }
+provider "aws" {
+  region = "us-east-1"
+}
+
+module "network" {
+  source = "./ecs/vpc.tf"
+}
+
+module "ecs_cluster" {
+  source = "./ecs/ecs_cluster.tf"
+}
+
+module "alb" {
+  source = "./ecs/alb.tf"
+}
+
+module "ecs_app" {
+  source = "./ecs/task_definition.tf"
+}
