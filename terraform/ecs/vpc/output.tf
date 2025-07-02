4136c9e06ec1e46631a30434804be46264608811
Rakesh Revashetti <122523208+rakeshrevashetti@users.noreply.github.com>
2025-07-03 02:50:51 +0530

    Create output.tf

---
+++ terraform/ecs/vpc/output.tf
@@ -0,0 +1,18 @@
+output "vpc_id" {
+  value = aws_vpc.main.id
+}
+
+output "public_subnet_ids" {
+  value = [
+    aws_subnet.public_a.id,
+    aws_subnet.public_b.id
+  ]
+}
+
+output "alb_sg_id" {
+  value = aws_security_group.alb_sg.id
+}
+
+output "ecs_sg_id" {
+  value = aws_security_group.ecs_sg.id
+}
