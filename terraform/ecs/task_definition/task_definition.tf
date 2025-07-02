dbcdf5a1970a8be2d2fa439160dcf6db388846ea
Rakesh Revashetti <122523208+rakeshrevashetti@users.noreply.github.com>
2025-07-03 03:26:02 +0530

    Update task_definition.tf

--- terraform/ecs/task_definition/task_definition.tf
+++ terraform/ecs/task_definition/task_definition.tf
@@ -20,7 +20,7 @@ resource "aws_ecs_task_definition" "app" {
         }
       ]
       healthCheck = {
-        command     = ["CMD-SHELL", "curl -f http://localhost:5000/health || exit 1"]
+        command     = ["CMD-SHELL", "curl -f http://localhost:5000/ || exit 1"]
         interval    = 30
         timeout     = 5
         retries     = 3
