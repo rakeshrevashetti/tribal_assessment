dd3987f947ff9a97a71de4fe4a44901c3c7158d9
Rakesh Revashetti <122523208+rakeshrevashetti@users.noreply.github.com>
2025-07-03 03:14:47 +0530

    Update alb.tf

--- terraform/ecs/alb/alb.tf
+++ terraform/ecs/alb/alb.tf
@@ -15,7 +15,7 @@ resource "aws_lb_target_group" "main" {
   vpc_id   = var.vpc_id
 
   health_check {
-    path                = "/health"
+    path                = "/"
     interval            = 30
     timeout             = 5
     healthy_threshold   = 2
