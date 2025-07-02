ac01af3a81b6d9546f777dd4befe28790efbe9e1
Rakesh Revashetti <122523208+rakeshrevashetti@users.noreply.github.com>
2025-07-03 01:45:29 +0530

    Create alb.tf

---
+++ terraform/ecs/alb/alb.tf
@@ -0,0 +1,43 @@
+
+resource "aws_lb" "main" {
+  name               = "tribal-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [var.alb_sg_id]
+  subnets            = var.public_subnet_ids
+}
+
+resource "aws_lb_target_group" "main" {
+  name     = "tribal-tg"
+  port     = 5000
+  protocol = "HTTP"
+  vpc_id   = var.vpc_id
+
+  health_check {
+    path                = "/health"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    matcher             = "200"
+  }
+}
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.main.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.main.arn
+  }
+}
+
+output "alb_dns_name" {
+  value = aws_lb.main.dns_name
+}
+
+output "target_group_arn" {
+  value = aws_lb_target_group.main.arn
+}
