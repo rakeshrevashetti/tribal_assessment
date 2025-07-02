23f274e2795eca25b4cec4e0490d48c8136805e2
Rakesh Revashetti <122523208+rakeshrevashetti@users.noreply.github.com>
2025-07-03 02:02:56 +0530

    Update variable.tf

--- terraform/ecs/vpc/variable.tf
+++ terraform/ecs/vpc/variable.tf
@@ -7,3 +7,15 @@ variable "vpc_name" {
   description = "Name tag for the VPC"
   type        = string
 }
+
+variable "vpc_cidr" {}
+variable "vpc_name" {}
+
+variable "public_subnet_a_cidr" {}
+variable "public_subnet_b_cidr" {}
+
+variable "availability_zone_a" {}
+variable "availability_zone_b" {}
+
+variable "public_subnet_a_name" {}
+variable "public_subnet_b_name" {}
