a16a31d5e263f9b4e1912ba58a53b127284189fd
Rakesh Revashetti <122523208+rakeshrevashetti@users.noreply.github.com>
2025-07-03 02:56:28 +0530

    Update vpc.tf

--- terraform/ecs/vpc/vpc.tf
+++ terraform/ecs/vpc/vpc.tf
@@ -90,18 +90,3 @@ resource "aws_security_group" "ecs_sg" {
   }
 }
 
-output "vpc_id" {
-  value = aws_vpc.main.id
-}
-
-output "public_subnet_ids" {
-  value = [aws_subnet.public_a.id, aws_subnet.public_b.id]
-}
-
-output "alb_sg_id" {
-  value = aws_security_group.alb_sg.id
-}
-
-output "ecs_sg_id" {
-  value = aws_security_group.ecs_sg.id
-}
