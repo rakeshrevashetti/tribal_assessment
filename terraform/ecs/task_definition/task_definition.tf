dfb2be2d391c15a1f9ee74c1f7dc6a40ed39ca07
Rakesh Revashetti <122523208+rakeshrevashetti@users.noreply.github.com>
2025-07-03 02:36:25 +0530

    Update task_definition.tf

--- terraform/ecs/task_definition/task_definition.tf
+++ terraform/ecs/task_definition/task_definition.tf
@@ -5,7 +5,8 @@ resource "aws_ecs_task_definition" "app" {
   network_mode             = "awsvpc"
   cpu                      = "256"
   memory                   = "512"
-  execution_role_arn       = var.task_exec_role_arn
+  execution_role_arn       = execution_role_arn = aws_iam_role.ecs_task_execution_role.arn
+
 
   container_definitions = jsonencode([
     {
