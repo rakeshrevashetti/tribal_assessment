4bca1e21c3564f47a53e14645a4eef79f8236a4e
Rakesh Revashetti <122523208+rakeshrevashetti@users.noreply.github.com>
2025-07-03 20:57:02 +0530

    Update deployment_script.sh

--- deployment_script.sh
+++ deployment_script.sh
@@ -3,7 +3,7 @@ AWS_REGION="us-east-1"
 REPO_NAME="flask-api"
 IMAGE_TAG="latest"
 ACCOUNT_ID=$(aws sts get-caller-identity --query Account --output text)
-docker build -t $REPO_NAME . 
+docker build -t $REPO_NAME ./python-api 
 aws ecr get-login-password --region $AWS_REGION | docker login --username AWS --password-stdin $ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com
 
 aws ecr describe-repositories --repository-names $REPO_NAME --region $AWS_REGION >/dev/null || aws ecr create-repository --repository-name $REPO_NAME --region $AWS_REGION
